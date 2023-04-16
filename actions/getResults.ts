2cfbcf0ce1abbda4c35841693281b4251b092140
raymondtju <raymond.rtju@gmail.com>
2023-04-16 23:24:27 +0700

    add revalidate into upstash

--- actions/getResults.ts
+++ actions/getResults.ts
@@ -1,5 +1,3 @@
-export const revalidate = 0;
-
 export default async function getResults() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/results/api`, {
--- lib/upstash.ts
+++ lib/upstash.ts
@@ -1,5 +1,7 @@
 import { Redis } from "@upstash/redis";
 
+export const revalidate = 0;
+
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
