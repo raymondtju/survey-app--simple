ae494bf04eb5afe1fb97c1b6cb317d8147f7c4cc
raymondtju <raymond.rtju@gmail.com>
2023-04-16 23:35:17 +0700

    try add revalidating into fetch api

--- actions/getResults.ts
+++ actions/getResults.ts
@@ -1,7 +1,10 @@
 export default async function getResults() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/results/api`, {
-      cache: "no-cache",
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      next: {
+        revalidate: 1,
+      },
     });
     const data = await res.json();
     return data.data;
--- lib/upstash.ts
+++ lib/upstash.ts
@@ -1,7 +1,5 @@
 import { Redis } from "@upstash/redis";
 
-export const revalidate = 0;
-
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
