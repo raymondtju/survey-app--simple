e4dccc7bede1730d355c42201420fdc2b8b555fc
raymondtju <raymond.rtju@gmail.com>
2023-04-16 12:05:08 +0700

    init

---
+++ components/ResultsPage.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ResultsPage() {
+  const [surveyData, setSurveyData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/results")
+      .then((res) => res.json())
+      .then((response) => setSurveyData(response.data));
+  }, []);
+  return (
+    <div>
+      {" "}
+      {surveyData.map((data) => (
+        <div key={data.id}>
+          <p>
+            <strong> Rating: </strong> {data.rating}{" "}
+          </p>{" "}
+          <p>
+            <strong> Recommendation: </strong> {data.recommendation}{" "}
+          </p>{" "}
+          <p>
+            <strong> Comment: </strong> {data.comment}{" "}
+          </p>{" "}
+        </div>
+      ))}{" "}
+    </div>
+  );
+}
